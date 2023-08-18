d50d91bd00186ec6d0c4f49be9d68c9360201162
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-18 15:08:38 +0530

    main application

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const user = require('./api/user.api');
+const userAddress = require('./api/userAddress.api');
 
 const express = require('express');
 
@@ -8,6 +9,7 @@ const app = express();
 // app.use(express.json())
 
 app.use('/',user);
+app.use('/',userAddress);
 
 app.listen(3000,()=>{
     console.log("Server update");
